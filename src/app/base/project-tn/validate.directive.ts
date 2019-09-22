e3ae627b5ab93f0047026c2209180783123b87a5
野田竜希 <tatsuki@tatsuki-noda.local>
2019-09-23 00:54:08 +0900

    first commit

--- src/app/base/project-tn/validate.directive.ts
+++ src/app/base/project-tn/validate.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, forwardRef } from '@angular/core';
+import { Directive, forwardRef, Input } from '@angular/core';
 import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
 
 @Directive({
@@ -12,10 +12,14 @@ import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
 export class ValidateDirective implements Validator {
 
   constructor() { }
+  @Input()
+  default: string;
 
   validate(control: AbstractControl) {
     if (control.value === '') {
       return { required: false };
+    } else if (this.default === control.value) {
+      return { pristine: false };
     }
   }
 }
